f17e4e60b731875f4b4e7d295696fe20e1d543d9
marrcelosantana <marcelosantana@alu.ufc.br>
2021-06-23 00:19:39 -0300

    Att - Atividade05

---
+++ atividade05/readme.md
@@ -0,0 +1,15 @@
+## TELAS
+
+### Home
+
+<img src="././prints/home.jpg">
+
+
+### Home with activities
+
+<img src="././prints/activities.jpg">
+
+
+### Activity selected
+
+<img src="././prints/selected.jpg">
--- atividade05/scripts/script.js
+++ atividade05/scripts/script.js
@@ -1,6 +1,6 @@
-class Activity{ 
+class Activity {
 
-    constructor(description, status){
+    constructor(description, status) {
         this.description = description;
         this.status = status;
     }
@@ -8,24 +8,25 @@ class Activity{
 
 let activities = [];
 
-function addActivity(){
+function addActivity() {
     let description = document.getElementById('input-add').value;
     activities.push(new Activity(description, false));
+
 }
 
-function clearInput(){
+function clearInput() {
     let description = document.getElementById('input-add');
     description.value = "";
 }
 
-function listActivities(){
+function listActivities() {
     let tbody = document.getElementById('tbody');
     tbody.innerText = "";
     activities.forEach(activity => {
         let index = activities.indexOf(activity);
         let tr = tbody.insertRow();
-        tr.setAttribute("onclick","completeActivity("+ index +"), listActivities()")
-        activity.status ? tr.classList.add('activityCompleted') :  tr.classList.add('activity')
+        tr.setAttribute("onclick", "completeActivity(" + index + "), listActivities()")
+        activity.status ? tr.classList.add('activityCompleted') : tr.classList.add('activity')
         let td_description = tr.insertCell();
         let td_action = tr.insertCell();
 
@@ -34,31 +35,40 @@ function listActivities(){
 
         let imgDelete = document.createElement('img');
         imgDelete.src = '../images/delete.svg';
-        imgDelete.setAttribute("onclick", "removeActivity("+ index +"), listActivities()");
-        
+        imgDelete.setAttribute("onclick", "removeActivity(" + index + "), listActivities()");
+
         let buttonDelete = document.createElement('Button');
         tr.classList.add('activity');
         buttonDelete.appendChild(imgDelete);
-        td_action.appendChild(buttonDelete);       
+        td_action.appendChild(buttonDelete);
     })
 }
 
-function findIndexActivity(value){
-    activities.forEach( activity => { if(activity === value)return activities.indexOf(activity)});
+function findIndexActivity(value) {
+    activities.forEach(activity => { if (activity === value) return activities.indexOf(activity) });
     return -1;
 }
 
-function removeActivity(index){
-    if(index >= 0){
+function removeActivity(index) {
+    if (index >= 0) {
         activities.splice(index, 1);
     }
 }
 
 
-function completeActivity(index){
-    if(index >= 0){
+function completeActivity(index) {
+    if (index >= 0) {
         activities[index].status = !activities[index].status;
     }
+}
+
+function pressEnter() {
+    const textbox = document.getElementById("input-add");
+    textbox.addEventListener("keypress", function onEvent(event) {
+        if (event.key === "Enter") {
+            document.getElementById("button-add").click();
+        }
+    });
 }
 
 
